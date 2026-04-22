90410fd5a1f82e56cf7b8883a7a8d21ad9db5f40
ScaccoFranco <samuelefrancoo@gmail.com>
2026-04-22 12:32:51 +0200

    esercitazione 5 primo commit

---
+++ esercitazione5/measure_sorts.cpp
@@ -0,0 +1,68 @@
+#include <ostream>
+#include <algorithm> 
+#include "sort.hpp"
+#include "timecounter.h"
+#include "randfiller.hpp"
+
+int main() {
+    randfiller rf;
+    sort s;
+    timecounter tc;
+
+    std::cout << '\n';
+
+    int len = 2;
+    while (len <= 8192) {
+
+        std::cout << len << " | ";
+        std::vector<int> vi;
+        double secs;
+
+        // BUBBLESORT
+        vi.resize(len);
+        rf.fill(vi, -500, 500);
+        tc.tic();
+        s.bubble(vi);
+        secs = tc.toc();
+        std::cout << "Bubblesort: " << secs << "s";
+
+
+        // INSERTIONSORT
+        vi.resize(len);
+        rf.fill(vi, -500, 500);
+        tc.tic();
+        s.insertion(vi);
+        secs = tc.toc();
+        std::cout << " | " << "Insertionsort: " << secs << "s";
+
+
+        // SELECTIONSORT    
+        vi.resize(len);
+        rf.fill(vi, -500, 500);
+        tc.tic();
+        s.selection(vi);
+        secs = tc.toc();
+        std::cout << " | " << "Bubblesort: " << secs << "s";
+
+
+        // MERGESORT
+        vi.resize(len);
+        rf.fill(vi, -500, 500);
+        tc.tic();
+        s.mergesort(vi, 0, len-1);
+        secs = tc.toc();
+        std::cout << " | " << "Mergesort: " << secs << "s";
+
+
+        // QUICKSORT
+        vi.resize(len);
+        rf.fill(vi, -500, 500);
+        tc.tic();
+        s.mergesort(vi, 0, len-1);
+        secs = tc.toc();
+        std::cout << " | " << "Quicksort: " << secs << "s\n";
+
+        len = len * 2;
+    }
+
+}
---
+++ esercitazione5/test_quicksort.cpp
@@ -0,0 +1,32 @@
+#include <iostream> // per cout
+#include "sort.hpp"
+#include "randfiller.hpp"
+
+int main() {
+    unsigned int N_TEST = 100; // numero di test
+    int MAX_LEN = 100; // massima lunghezzza dei vettori che genero casualmente
+    randfiller rf;
+    sort s;
+
+    for (unsigned int i = 0; i < N_TEST; i++) {
+        unsigned int len = rf.rand_int(0, MAX_LEN);
+
+        std::vector<int> vi;
+        vi.resize(len);
+        rf.fill(vi, -500, 500); 
+
+        s.quicksort(vi, 0, len-1);
+        if (!s.is_sorted(vi)) {
+            return EXIT_FAILURE;
+        }
+    }
+
+    std::vector<std::string> vs = {"ciao", "Ciao", "Ciao a tutti", "Ciaooooooo", "cIAO", "CIAO A TUTTIIIIII"};
+    s.insertion(vs);
+    s.print(vs);
+    if (!s.is_sorted(vs)) {
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
