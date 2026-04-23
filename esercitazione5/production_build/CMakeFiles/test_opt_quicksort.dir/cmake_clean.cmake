2032c644c3d4f2cfd1f4f9605a6743161522450f
ScaccoFranco <samuelefrancoo@gmail.com>
2026-04-23 13:11:08 +0200

    aggiunte build

---
+++ esercitazione5/production_build/CMakeFiles/test_opt_quicksort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_opt_quicksort.dir/test_opt_quicksort.cpp.o"
+  "CMakeFiles/test_opt_quicksort.dir/test_opt_quicksort.cpp.o.d"
+  "test_opt_quicksort"
+  "test_opt_quicksort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_opt_quicksort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ esercitazione5/production_build/CTestTestfile.cmake
@@ -0,0 +1,18 @@
+# CMake generated Testfile for 
+# Source directory: /home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5
+# Build directory: /home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[bubblesort]=] "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build/test_bubblesort")
+set_tests_properties([=[bubblesort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;19;add_test;/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;0;")
+add_test([=[insertionsort]=] "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build/test_insertionsort")
+set_tests_properties([=[insertionsort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;22;add_test;/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;0;")
+add_test([=[selectionsort]=] "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build/test_selectionsort")
+set_tests_properties([=[selectionsort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;25;add_test;/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;0;")
+add_test([=[mergesort]=] "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build/test_mergesort")
+set_tests_properties([=[mergesort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;28;add_test;/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;0;")
+add_test([=[quicksort]=] "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build/test_quicksort")
+set_tests_properties([=[quicksort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;31;add_test;/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;0;")
+add_test([=[opt-quicksort]=] "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/production_build/test_opt_quicksort")
+set_tests_properties([=[opt-quicksort]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;35;add_test;/home/samuele/Scrivania/UNI/corsi semestre 3.2/Programmazione e Calcolo/esercitazione5/CMakeLists.txt;0;")
